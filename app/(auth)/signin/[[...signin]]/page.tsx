9042cc76b9086cca2cbc3a1265193513b28ef1b4
bibekshhh <bibekrock2019@gmail.com>
2024-06-03 00:31:34 +0545

    [fix]: header buttons' redirects && ui improvements

--- app/(auth)/signin/[[...signin]]/page.tsx
+++ app/(auth)/signin/[[...signin]]/page.tsx
@@ -1,11 +1,13 @@
-import Login from "@/app/components/pagecomponents/login/Login";
+import Header from "@/app/components/globalcomponents/Header";
 import { SignIn } from "@clerk/nextjs";
 
 export default function SignInPage() {
   return (
-    <main className="min-h-screen flex items-center justify-center bg-slate-50">
-      <SignIn />
-      {/* <Login /> */}
-    </main>
+    <>
+      <Header />
+      <main className="min-h-[87.5vh] flex items-center justify-center bg-slate-50">
+        <SignIn />
+      </main>
+    </>
   );
 }
